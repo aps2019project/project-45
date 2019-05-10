e833a241a77738fb1aaa4124287beb4d00aee496
Lenovo <Aliseifkashani@ce.sharif.edu>
2019-05-11 00:55:12 +0430

    Merge branch 'master' of C:\Users\Lenovo\IdeaProjects\project-45 with conflicts.

--- src/controller/PlayController.java
+++ src/controller/PlayController.java
@@ -145,7 +145,7 @@ public class PlayController {
         Matcher showDeckMatcher = showDeckPattern.matcher(command);
         if (showDeckMatcher.matches()) {
             getRelatingCollection().showDeck(showDeckMatcher.group(1));
-        }//this is a new comment
+        } //new comment man
     } // need to be completed
     private void shopRequest(String command) {
         Pattern showCollectionPattern = Pattern.compile("show collection");
