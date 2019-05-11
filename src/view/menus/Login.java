c09cc05e4e17a336900f73f756a5a29387ca5b51
Ali Ehsani <49426035+ali93ehsan@users.noreply.github.com>
2019-05-11 06:00:51 +0430

    Complete Login 2

--- src/view/menus/Login.java
+++ src/view/menus/Login.java
@@ -3,6 +3,7 @@ package view.menus;
 import controller.PlayController;
 import model.Account;
 import model.MenuType;
+import model.PlayingType;
 import view.Battle;
 import view.Request;
 
@@ -17,40 +18,45 @@ public class Login extends Menu {
         return LOGIN;
     }
 
+    private String secondUserName;
+
     public void login() {
-
-
+        boolean inLogin = true;
         do {
-            System.out.println("Enter Your Username: ");
             Request requestUser = new Request(Login.getInstance());
-            requestUser.setNewCommand();
+            if (secondUserName != null) {
+                System.out.printf("second username : %s\n", secondUserName);
+            } else {
+                System.out.println("Enter Your Username: ");
+                requestUser.setNewCommand();
+                if (!requestUser.exitOrHelp(Login.getInstance(), requestUser.getCommand())) return;
+            }
+
             for (Account account : AccountMenu.accounts) {
                 if (account.getUserName().equals(requestUser.getCommand())) {
                     System.out.println("Enter Your Password: ");
-                    Request requestPass = new Request(new Login());
+                    Request requestPass = new Request(Login.getInstance());
                     requestPass.setNewCommand();
-                    if (requestPass.getCommand().equals(account.getPassWord())) {
-                        PlayController.menus.add(MainMenu.getInstance());
+                    if (!requestPass.exitOrHelp(Login.getInstance(), requestPass.getCommand())) return;
 
-                        return;
-                    }else {
-                        System.out.println("Invalid Username or Password!");
-                    }
-                    if (battle.getActiveAccounts()[0] == null) {
-                        battle.getActiveAccounts()[0] = account;
+                    if (requestPass.getCommand().equals(account.getPassWord())) {
+                        if (battle.getActiveAccounts()[0] == null) {
+                            battle.getActiveAccounts()[0] = account;
+                        } else {
+                            battle.getActiveAccounts()[1] = account;
+                            battle.getActiveAccounts()[0].setActiveAtTheMoment(false);
+                        }
+                        account.setActiveAtTheMoment(true);
+                        PlayController.menus.add(MainMenu.getInstance());
+                        inLogin = false;
                     } else {
-                        battle.getActiveAccounts()[1] = account;
-                        battle.getActiveAccounts()[0].setActiveAtTheMoment(false);
+                        if (secondUserName != null) System.out.println("Invalid password!");
+                        else System.out.println("Invalid Username or Password!");
                     }
-                    account.setActiveAtTheMoment(true);
-                    PlayController.menus.add(PlayingTypeMenu.getInstance());
-                }else {
-                    System.out.println("Invalid Username or Password!");
                 }
             }
-        } while (true);
 
-        //System.out.println("Invalid username!");
+        } while (inLogin);
     }
 
     @Override
@@ -67,5 +73,9 @@ public class Login extends Menu {
     public void open() {
         System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * Login Menu " +
                 "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+    }
+
+    public void setSecondUserName(String secondUserName) {
+        this.secondUserName = secondUserName;
     }
 }
